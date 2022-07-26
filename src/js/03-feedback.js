bfe3fd5e09c48d9ebd48feab54bed6ef63a8c542
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-07-26 04:58:59 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,53 +2,47 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', throttle(onFormInput, 500));
-const formData = localStorage.getItem('feedback-form-state')
-  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+const KEY = 'feedback-form-state';
+const formData = localStorage.getItem(KEY)
+  ? JSON.parse(localStorage.getItem(KEY))
   : {};
 
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  const keyParsed = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = e.currentTarget.elements.email.value;
-  const message = e.currentTarget.elements.message.value;
+reloadPage();
 
-  if (email === '' || message === '') {
-    return alert('Заповни поля');
+function reloadPage() {
+  const savedFormData = JSON.parse(localStorage.getItem(KEY));
+
+  if (savedFormData) {
+    if (savedFormData.email) {
+      form.elements.email.value = savedFormData.email;
+    }
+    if (savedFormData.message) {
+      form.elements.message.value = savedFormData.message;
+    }
   }
-  console.log(keyParsed);
-
-  e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
 }
 
 function onFormInput({ target }) {
   formData[target.name] = target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(KEY, JSON.stringify(formData));
 }
 
-function setFormValues() {
-  const savedInfo = localStorage.getItem('feedback-form-state');
-  console.log(savedInfo);
+function onFormSubmit(e) {
+  e.preventDefault();
 
-  if (savedInfo) {
-    const data = JSON.parse(savedInfo);
-    if (data.email) {
-      form.elements.email.value = data.email;
-    }
-    if (data.message) {
-      form.elements.message.value = data.message;
-    }
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  if (email === '' || message === '') {
+    return alert('Заповніть будь-ласка всі поля');
   }
+
+  const parsedData = JSON.parse(localStorage.getItem(KEY));
+  console.log(parsedData);
+
+  localStorage.removeItem(KEY);
+  e.target.reset();
 }
-
-setFormValues();
-
-// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
-
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
-
-// При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
-// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
